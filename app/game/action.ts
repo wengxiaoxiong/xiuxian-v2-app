f32a5b15d97868a9117ec2326c74316179049550
wengxiaoxiong <owner@wengxiaoxiong.com>
2025-05-14 21:22:36 +0800

    推进模块完成

--- app/game/action.ts
+++ app/game/action.ts
@@ -3,12 +3,13 @@ import { z } from "zod"
 import { CharacterDescriptionSchema, CharacterStatusSchema, getCharacterById, updateCharacterStatus } from "../character/action";
 import deepseek from "@/utils/deepseek";
 import { generateObject } from "ai";
-import {  PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "../generated/prisma";
 
 
 const prisma = new PrismaClient()
 
 const story_prompt = "story_prompt"
+const summary_prompt = "summary_prompt"
 
 const storyPushSchema = z.object({
     节点要素: z.object({
@@ -51,7 +52,22 @@ const storyPushSchema = z.object({
 
 export type StoryPushType = z.infer<typeof storyPushSchema>;
 
+const summaryPushSchema = z.object({
+    剧情要素: z.object({
+        描述: z.string(),
+        场景: z.array(z.string()),
+        人物: z.array(z.string()),
+    })
+})
 
+export type SummaryPushType = z.infer<typeof summaryPushSchema>;
+
+
+/**
+ * @Action 开始游戏
+ * @param characterId 角色id
+ * @returns 
+ */
 export async function startGame(characterId: number) {
     const character = await getCharacterById(characterId)
     const characterStatusSchema = CharacterStatusSchema.safeParse(character.status)
@@ -75,8 +91,18 @@ export async function startGame(characterId: number) {
 
     console.log(dynamicInput)
 
-    let prompt = story_prompt.replace("{CHARACTER_DESCRIPTION}", JSON.stringify(characterDescription))
-    prompt = story_prompt.replace("{DYNAMIC_INPUT}", dynamicInput)
+
+    const dict = await prisma.dictionary.findFirst({
+        where: {
+            key: story_prompt
+        }
+    })
+
+    if (!dict) {
+        throw new Error("没有找到Prompt角色描述")
+    }
+    let prompt = dict.value.replace("{CHARACTER_DESCRIPTION}", JSON.stringify(characterDescription))
+    prompt = prompt.replace("{DYNAMIC_INPUT}", dynamicInput)
 
     const { object: gamePush } = await generateObject({
         model: deepseek("deepseek-chat"),
@@ -107,6 +133,155 @@ export async function startGame(characterId: number) {
     console.log(`
         更新后状态: ${JSON.stringify(newStatus)}
     `)
+
+    return {
+        id: game.id,
+        gamePush: gamePush,
+        newStatus: newStatus
+    }
+}
+
+/**
+ * @Action 推进游戏
+ * @param gameId 游戏id
+ * @param characterId 角色id
+ * @param gamePush 游戏推送到
+ * @returns 
+ */
+export async function pushGame(gameId: number, characterId: number, choice: string) {
+
+    const game = await prisma.game.findFirst({
+        where: {
+            id: gameId
+        },
+        include: {
+            gamePush: true
+        }
+    })
+
+    if (!game) {
+        throw new Error("游戏不存在")
+    }
+
+    // gamePush不能为空
+    if (!game.gamePush) {
+        throw new Error("游戏推送到不能为空")
+    }
+
+    let gameContext = game.gamePush.map((push) => push.push).join("\n")
+
+    // 判断game.gamePush是否超过3个节点，若超过则该浓缩剧情
+    if (game.gamePush.length > 3) {
+
+        const dict = await prisma.dictionary.findFirst({
+            where: {
+                key: summary_prompt
+            }
+        })
+
+        if (!dict) {
+            throw new Error("没有找到Prompt角色描述")
+        }
+
+        const prompt = dict.value
+
+        const { object: summaryPush } = await generateObject({
+            model: deepseek("deepseek-chat"),
+            maxTokens: 3000,
+            schema: summaryPushSchema,
+            prompt: prompt
+        })
+
+        console.log(`
+            浓缩剧情: ${JSON.stringify(summaryPush)}
+        `)
+
+
+        gameContext = JSON.stringify(summaryPush)
+        
+        // 降原本的3个push清空，添加新的summaryPush
+        await prisma.gamePush.deleteMany({
+            where: {
+                gameId: gameId
+            }
+        })
+
+        await prisma.gamePush.create({
+            data: {
+                gameId: gameId,
+                push: summaryPush
+            }
+        })
+
+    }
+
+
+    const character = await getCharacterById(characterId)
+    const characterStatusSchema = CharacterStatusSchema.safeParse(character.status)
+    if (characterStatusSchema.data?.是否死亡) {
+        throw new Error("角色已死亡")
+    }
+
+    // 去除description中的'初始属性'和'人物年龄'字段
+    const characterDescriptionSchema = CharacterDescriptionSchema.safeParse(character.description)
+    if (!characterDescriptionSchema.success) {
+        throw new Error("角色描述解析失败")
+    }
+    const { 初始属性, 人物年龄, ...restDescription } = characterDescriptionSchema.data
+    const characterDescription = restDescription
+
+    const currentStatus = characterStatusSchema.data
+
+    const dynamicInput = `
+    当前角色状态: ${JSON.stringify(currentStatus)}
+    玩家选择: ${choice}
+    游戏上下文: ${gameContext}
+    `
+
+    console.log(dynamicInput)
+
+    const dict = await prisma.dictionary.findFirst({
+        where: {
+            key: story_prompt
+        }
+    })
+
+    if (!dict) {
+        throw new Error("没有找到Prompt角色描述")
+    }
+
+    let prompt = dict.value
+    prompt = prompt.replace("{CHARACTER_DESCRIPTION}", JSON.stringify(characterDescription))
+    prompt = prompt.replace("{DYNAMIC_INPUT}", dynamicInput)
+
+    const { object: gamePush } = await generateObject({
+        model: deepseek("deepseek-chat"),
+        maxTokens: 3000,
+        schema: storyPushSchema,
+        prompt: prompt
+    })
+
+    // 更新角色状态
+    const newStatus = await updateCharacterStatus(characterId, gamePush)
+
+    console.log(`
+        更新后状态: ${JSON.stringify(newStatus)}
+    `)
+
+    // 更新游戏状态
+    await prisma.game.update({
+        where: {
+            id: gameId
+        },
+        data: {
+            gamePush: {
+                create: {
+                    push: gamePush
+                }
+            }
+        }
+    })
+    
 
     return {
         id: game.id,
